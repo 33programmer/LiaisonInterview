9fa55aa9e782366f5703ee507f68015e32fbf478
Akeme Ubom <programmer33@outlook.com>
2018-01-12 11:46:33 -0500

    Adding a new method
    Cleaning up and adding a new method.

--- jboss7-interview-webapp/src/main/java/org/jboss/tools/examples/data/DeveloperRepository.java
+++ jboss7-interview-webapp/src/main/java/org/jboss/tools/examples/data/DeveloperRepository.java
@@ -28,7 +28,7 @@ public class DeveloperRepository {
         Root<Developer> developer = criteria.from(Developer.class);
         // Swap criteria statements if you would like to try out type-safe criteria queries, a new
         // feature in JPA 2.0
-        // criteria.select(member).where(cb.equal(member.get(Member_.name), email));
+        // criteria.select(member).where(cb.equal(developer.get(Developer_.name), email));
         criteria.select(developer).where(cb.equal(developer.get("email"), email));
         return em.createQuery(criteria).getSingleResult();
     }
@@ -39,8 +39,19 @@ public class DeveloperRepository {
         Root<Developer> developer = criteria.from(Developer.class);
         // Swap criteria statements if you would like to try out type-safe criteria queries, a new
         // feature in JPA 2.0
-        // criteria.select(member).orderBy(cb.asc(member.get(Member_.name)));
+        // criteria.select(member).orderBy(cb.asc(developer.get(Developer_.name)));
         criteria.select(developer).orderBy(cb.asc(developer.get("name")));
         return em.createQuery(criteria).getResultList();
+    }
+        
+    // Good to have in the future
+      public Developer findByManager(String manager) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Developer> criteria = cb.createQuery(Developer.class);
+        Root<Developer> developer = criteria.from(Developer.class);
+        // Swap criteria statements if you would like to try out type-safe criteria queries, a new
+        // feature in JPA 2.0
+        criteria.select(developer).where(cb.equal(developer.get("manager"), manager)); // QUERY WHERE manager=''
+        return em.createQuery(criteria).getSingleResult();
     }
 }
